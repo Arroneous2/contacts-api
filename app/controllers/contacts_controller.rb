36b73179b9210380f8245a876d3f3f91865e926b
Arroneous2 <thumpinbassbeat@gmail.com>
2024-05-13 13:58:49 -0500

    new and create methods for ContactsController

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,4 +6,20 @@ class ContactsController < ApplicationController
   def show
     @contact = Contact.find(params[:id])
   end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(last_name: "...", first_name: "...", email: "...", phone_number: "...")
+
+    if @contact.save
+      redirect_to @contact
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
 end
